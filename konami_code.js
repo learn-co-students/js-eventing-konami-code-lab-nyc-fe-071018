069154fecc90c36e9b22ea34839cdafbcfdff5e3
Kiki Dolan <cdolan323@gmail.com>
2018-08-22 01:02:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,51 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function onKeyDownHandler(event) {
+  console.log(tracker)
+  const key = parseInt(event.detail || event.which)
+  if (tracker === code.length) {
+    event.preventDefault()
+    return
+  }
+if (key === code[tracker]) {
+  tracker++
+  if (tracker === code.length) {
+    window.alert("YAS!")
+  }
+} else {
+  console.log('BUMMAH!')
+  tracker = 0
 
 }
+}
+
+function init() {
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', onKeyDownHandler)
+}
+
+init()
+
+//function init() {
+  //let tracker = 0
+  // Write your JavaScript code inside the init() function
+//const body = document.querySelector('body')
+//body.addEventListener('keydown', (event) => {
+  //if (tracker === code.length) {
+    //event.preventDefault()
+    //return
+  //}
+  //if (event.which === code[tracker]) {
+    //console.log('YAS!')
+   // tracker ++
+  //  if (tracker === code.length) {
+    //  alert ('BOOM!')
+    //}
+//  } else {
+  //  console.log('BUMMAH!')
+  //  tracker = 0
+ // }
+//})
+//}
+
